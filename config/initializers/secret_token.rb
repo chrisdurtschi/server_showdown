d92781d3b8a2a004c6a3bd006234e6cf60d4b6b3
Christopher Durtschi <christopher.durtschi@gmail.com>
2012-05-07 22:50:07 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerShowdown::Application.config.secret_token = 'b8c98d982c06da737fa117a31e3dd017ca6d25a39475425b03ea639617840221ec24aa70f20d1e3bf1a0399d2872d279153bfec9afafa6124201df409ce36e70'
